69ba4d4170e48733f728bb17dd0fedd26cbb10f5
Luke Sivulka <lukesiv146@icloud.com>
2023-12-18 10:41:39 -0600

    fslfskj

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -2,10 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Kids Eat Free")
-                .font(.largeTitle)
-                .fontWeight(.black)
+        NavigationView {
+            VStack(alignment: .center, spacing: 10.0) {
+                Text("Kids Eat Free")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                Spacer()
+
+                NavigationLink(destination: DaysOfWeekView()) {
+                    Text("See Some Deals")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(50)
+                }
+            }
+            .padding()
+            .shadow(radius: 20)
+            .navigationBarTitle("Home Page", displayMode: .inline)
+            
         }
+        .shadow(radius: 20)
     }
 }
