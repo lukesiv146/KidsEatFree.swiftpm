16f7618e9a5b28389f480382c1002c0091a4e880
Luke Sivulka <lukesiv146@icloud.com>
2023-12-18 10:19:14 -0600

    aspiahsfpaoih

---
+++ Views/DaysOfWeekView.swift
@@ -0,0 +1,20 @@
+//
+//  DaysOfWeekView.swift
+//  KidsEatFree
+//
+//  Created by Luke J. Sivulka on 12/18/23.
+//
+
+import SwiftUI
+
+struct DaysOfWeekView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DaysOfWeekView_Previews: PreviewProvider {
+    static var previews: some View {
+        DaysOfWeekView()
+    }
+}
