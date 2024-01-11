e9e1b8eb6b28680e749859dd0b08aeb1f5f59716
Luke Sivulka <lukesiv146@icloud.com>
2024-01-11 11:14:59 -0600

    added image

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -1,27 +1,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
         NavigationView {
             VStack(alignment: .center, spacing: 10.0) {
+               
+                    
                 Text("Kids Eat Free")
                     .font(.largeTitle)
                     .fontWeight(.black)
                 Spacer()
-
+                
+                Image("kids")
+                    .scaledToFit()
+                Spacer()
+                
                 NavigationLink(destination: DaysOfWeekView()) {
                     Text("See Some Deals")
                         .foregroundColor(.white)
                         .padding()
                         .background(Color.blue)
                         .cornerRadius(50)
+                    
                 }
             }
             .padding()
-            .shadow(radius: 20)
             .navigationBarTitle("Home Page", displayMode: .inline)
             
         }
         .shadow(radius: 20)
     }
+    
+   
 }
+
